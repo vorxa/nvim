e5def600339c3071bdc2d05b0580b4e69542a7be
vorxa <vorxa@yandex.ru>
2024-03-19 15:25:04 +0700

    20240319-1524

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,11 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "html",
+      "xml",
+      "yaml",
+      "json",
+      "javascript",
     })
   end,
 }
